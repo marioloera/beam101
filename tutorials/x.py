9638e5eb897f91f8c3c2899908d7a1fd1bc35590
Mario Loera <mario.loera@trustly.com>
2021-03-31 20:55:13 +0200

    TaggedOutput

--- tutorials/x.py
+++ tutorials/x.py
@@ -7,7 +7,6 @@ import argparse
 
 def even_odd(x):
    type = 'odd' if x % 2 else 'even'
-   #yield (type, x)
    yield pvalue.TaggedOutput(type, x)
    if x % 10 == 0:
      yield x
@@ -23,24 +22,13 @@ if __name__ == '__main__':
    numbers = [i for i in range(23)]
    print('numbers:', numbers)
 
-   results = numbers | beam.FlatMap(even_odd).with_outputs()
+   results = numbers | beam.FlatMap(even_odd).with_outputs(
+      'odd', 'even'
+   )
 
    print('results:\n', results)
    print('results[None]:\n', results[None])
-
-
-   print(1, ' r:\n', results._results_by_tag)
-   print(2, ' r:\n', results._results_by_tag.items())
-   #output1 = (results | beam.Map(print))
-
-   # evens = results.even
-   # odds = results.odd
-   # tens = results[None]  # the undeclared main output
-
-   # print(evens)
-   # print(odds)
-   # print(tens)
-
-
+   print('results["even"]:\n', results['even'])
+   print('results["odd"]:\n', results['odd'])
 
    p.run().wait_until_finish()
