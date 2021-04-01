6bf206521f0bcec15a847248b93d1fb4edd9fe9b
Mario Loera <mario.loera@trustly.com>
2021-04-01 16:33:30 +0200

    Getstarted_Learnthebasics.py

---
+++ tutorials/Getstarted_Learnthebasics.py
@@ -0,0 +1,71 @@
+import apache_beam as beam
+
+inputs = [0, 1, 2, 3, 4]
+
+
+# FILTER
+def isEven(x):
+  return x % 2 == 0
+
+with beam.Pipeline() as pipeline:
+
+  """
+    outputs = pipeline | step1 | step2 | step3
+    Pipelines can quickly grow long, so it's sometimes easier to read if we surround them with parentheses and break them into multiple lines.
+
+    # This is equivalent to the example above.
+    outputs = (
+      pipeline
+      | step1
+      | step2
+      | step3
+    )
+  """
+
+  outputs = (
+      pipeline
+      # We use the Create transform to feed the pipeline with an iterable of elements, like a list.
+      # name must be uniques
+      | 'Create values' >> beam.Create(inputs)
+      | 'Keep only even numbers' >> beam.Filter(lambda x: x % 2 == 0)
+      # same ways to do it
+      #| 'Keep only even numbers 2' >>beam.Filter(isEven)  # direct call of the function
+      #| 'Keep only even numbers 3' >> beam.Filter(lambda x: isEven(x)) # lamda funtion
+
+
+      | 'Expand elements' >> beam.FlatMap(lambda x: [x for _ in range(x)])
+
+      #| 'Expand elements' >> beam.Map(lambda x: [x for _ in range(x)])
+      # using only Flat
+      # [1]
+      # [2, 2]
+      # [3, 3, 3]
+
+
+      # names can be skiped
+      | 'Multiply by 3' >> beam.Map(lambda x: x * 3.7)
+      | 'Sum all values together' >> beam.CombineGlobally(sum)
+
+  )
+
+  # We can only access the elements through another transform.
+  outputs | beam.Map(print)
+
+
+
+inputs = [
+  ('🐹', '🌽'),
+  ('🐼', '🎋'),
+  ('🐰', '🥕'),
+  ('🐹', '🌰'),
+  ('🐰', '🥒'),
+]
+
+with beam.Pipeline() as pipeline:
+  outputs = (
+      pipeline
+      | 'Create (animal, food) pairs' >> beam.Create(inputs)
+      | 'Group foods by animals' >> beam.GroupByKey()
+  )
+
+  outputs | beam.Map(print)
