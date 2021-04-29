1d55791f7027835541510df2388bbc2ae9cf0163
Mario Loera <mario.loera@trustly.com>
2021-04-29 17:54:07 +0200

    works

--- tobq/beam_to_bq_streaming_nTables_nSchemas.py
+++ tobq/beam_to_bq_streaming_nTables_nSchemas.py
@@ -15,11 +15,12 @@ def run(argv):
 
     project_id = "trustly-ds-test-1"
     dataset_id = "mario24"
+    gcs_bucket = "gs://trustly-ds-test-1_mario24"
 
     tables_kv_pairs = [
-        ("error", f"{project_id}:{dataset_id}.error_table8"),
-        ("user_log", f"{project_id}:{dataset_id}.query_table8"),
-        ("event", f"{project_id}:{dataset_id}.event_table8"),
+        ("error", f"{project_id}:{dataset_id}.error_table9"),
+        ("user_log", f"{project_id}:{dataset_id}.query_table9"),
+        ("event", f"{project_id}:{dataset_id}.event_table9"),
     ]
 
     data = [
@@ -36,14 +37,14 @@ def run(argv):
         {
             "type": "event",
             "timestamp": str(datetime.datetime.now()),
-            "message2": "loggin"
+            "message3": "loggin"
         },
     ]
 
     schema1 = {
         "fields": [
             {"name": "type", "type": "STRING", "mode": "NULLABLE"},
-            {"name": "timestamp", "type": "TIMESTAMP", "mode": "NULLABLE"},
+            {"name": "timestamp", "type": "STRING", "mode": "NULLABLE"},
             {"name": "message1", "type": "STRING", "mode": "NULLABLE"},
         ]
     }
@@ -51,7 +52,7 @@ def run(argv):
     schema2 = {
         "fields": [
             {"name": "type", "type": "STRING", "mode": "NULLABLE"},
-            {"name": "timestamp", "type": "TIMESTAMP", "mode": "NULLABLE"},
+            {"name": "timestamp", "type": "STRING", "mode": "NULLABLE"},
             {"name": "message2", "type": "STRING", "mode": "NULLABLE"},
         ]
     }
@@ -59,7 +60,7 @@ def run(argv):
     schema3 = {
         "fields": [
             {"name": "type", "type": "STRING", "mode": "NULLABLE"},
-            {"name": "timestamp", "type": "TIMESTAMP", "mode": "NULLABLE"},
+            {"name": "timestamp", "type": "STRING", "mode": "NULLABLE"},
             {"name": "message3", "type": "STRING", "mode": "NULLABLE"},
         ]
     }
@@ -67,19 +68,21 @@ def run(argv):
     schema_kv_pairs = [
         (tables_kv_pairs[0][1], schema1),
         (tables_kv_pairs[1][1], schema2),
-        (tables_kv_pairs[1][1], schema3),
+        (tables_kv_pairs[2][1], schema3),
     ]
 
     # check mapping
     for s in schema_kv_pairs:
         print(s[0], s[1]["fields"][2]["name"])
-   
-    sys.exit(0)
+
     with beam.Pipeline() as p:
 
         # create p collection from data
         elements = p | "add data" >> beam.Create(data)
         # elements | "print data" >> beam.Map(print)
+
+        schema_map_pcv = beam.pvalue.AsDict(
+            p | "MakeSchemas" >> beam.Create(schema_kv_pairs))
 
         table_record_pcv = beam.pvalue.AsDict(
             p | "MakeTables" >> beam.Create(tables_kv_pairs))
@@ -87,8 +90,11 @@ def run(argv):
         elements | WriteToBigQuery(
             table=lambda row, table_dict: table_dict[row["type"]],
             table_side_inputs=(table_record_pcv,),
-            method=WriteToBigQuery.Method.STREAMING_INSERTS,
-            schema=schema_
+            schema=lambda dest,
+            schema_map: schema_map.get(dest, None),
+            schema_side_inputs=(schema_map_pcv, ),
+            # method=WriteToBigQuery.Method.STREAMING_INSERTS
+            custom_gcs_temp_location=gcs_bucket
         )
 
 
